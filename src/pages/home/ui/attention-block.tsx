9914110b6d0c01f8db2df6093f9b8b001f02fb97
Un1T3G <un1t3gx@gmail.com>
2025-03-15 08:19:24 +0500

    fix

--- src/pages/home/ui/attention-block.tsx
+++ src/pages/home/ui/attention-block.tsx
@@ -5,7 +5,9 @@ export const AttentionBlock = () => {
   return (
     <Card className="p-4">
       <CardDescription className="flex items-center space-x-2">
-        <CircleAlert className="text-red-500" />
+        <div>
+          <CircleAlert className="text-red-500" />
+        </div>
         <span>
           Внимание: Сервер работает на картошке 🥔! Возможно всё бдеть
           подгружаться медленно {':('}
