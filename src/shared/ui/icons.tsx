d80eeac2105bd40a7e19ada8415e8d0a3dfc8de1
Un1T3G <un1t3gx@gmail.com>
2025-09-16 19:22:38 +0500

    update

--- src/shared/ui/icons.tsx
+++ src/shared/ui/icons.tsx
@@ -9,7 +9,8 @@ interface IProps {
 }
 
 export const Icons = ({ icon, className }: IProps) => {
-  const IconComponent = LucideIcons[icon] as JSX.ElementType
+  const IconComponent =
+    (LucideIcons[icon] as JSX.ElementType) || LucideIcons.AlertCircle
 
   return <IconComponent className={className} />
 }
